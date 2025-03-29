1b372c19ceeec7d92eb4467358de17e866834541
Cesar Nascimento <dev.cesar.nascimento+gh@gmail.com>
2025-03-28 22:47:29 -0300

    feat: configure cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const tripsRouter = require("./routes/trip");
@@ -9,6 +10,7 @@ const usersRouter = require("./routes/users");
 
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
